227dacec470bbd472b9577d491bb9a0f5c37ae5d
luisigarciia <garcial@udesa.edu.ar>
2022-04-11 11:52:51 -0300

    imagenes

--- db/producto.js
+++ db/producto.js
@@ -1,61 +1,61 @@
 const producto={
     listaProductos:[
         {
-            image: "/images/products/",
+            image: "/images/products/fotoproducto.jpg",
             product:"Labial Carmex",
             description:"Bálsamo reparador de labios presentacion en tubo por unidad (no blister)",
             date: "03/07/2021"
         },
         {
-            image: "/images/products/",
+            image: "/images/products/theordinary.jpg",
             product:"Acido Hialuronico The Ordinary",
             description:"Niacinamide 10% + Zinc 1%",
             date: "04/04/2022"
         },
         {
-            image: "/images/products/",
+            image: "/images/products/limpiadorfacial.jpg",
             product:"Limpiador Facial Cetaphil",
             description:"Loción Limpiadora para Piel Grasa x 237ml",
             date: "02/11/2021"
         },
         {
-            image: "/images/products/",
+            image: "/images/products/revitalift.jpg",
             product:"Revitalift Loreal",
             description:"Acido Hialuronico",
             date: "19/02/2022"
         },
         {
-            image: "/images/products/",
+            image: "/images/products/aguaMicelar.jpg",
             product:"Agua Micelar Garnier",
             description:"Agua Micelar Garnier Skin Active x 400 ml",
             date: "11/04/2022"
         },
         {
-            image: "/images/products/",
+            image: "/images/products/clearwash.png",
             product:"Clear Wash Neutrogena",
             description:"Gel limpiador facial Neutrogena",
             date: "24/12/21"
         },
         {
-            image: "/images/products/",
+            image: "/images/products/neutrogenaHidroboost.jpg",
             product:"Neutrogena Hydro Boost ",
             description:"Gel en crema extra seco",
             date: "06/11/22"
         },
         {
-            image: "/images/products/",
+            image: "/images/products/chapstick.jpg",
             product:"Balsamo labial Chap Stick ",
             description:"Balsamo labial clasico",
             date: "06/05/2020"
         },
         {
-            image: "/images/products/",
+            image: "/images/products/toallitasdesmaquillantes.jpg",
             product:"Removedor de maquillaje Neutrogena",
             description:"Toallas humedas limpiadoras de maquillaje",
             date: "22/11/2021"
         },
         {
-            image: "/images/products/",
+            image: "/images/products/eucerin.jpg",
             product:"Protector solar Eucerin Oil Control",
             description:"Toque Seco en gel-crema FPS50 x 50 ml",
             date: "11/10/2022"
