fecc8ebeca534178d932c5a129b4c07a32d9b3d9
luisigarciia <garcial@udesa.edu.ar>
2022-04-11 10:33:44 -0300

    cambio appjs

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var productRouter = require('./routes/product');
-const searchResultsRouter = require('./routes/searchResults');
 
 var app = express();
 
@@ -24,7 +23,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/product', productRouter);
-app.use('/searchresults', searchResultsRouter);
 
 
 // catch 404 and forward to error handler
