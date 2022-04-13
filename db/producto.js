8c7774807de1d5987fdc18a24516abd4d02ab245
Maria Sicco <mjsicco@hotmail.com>
2022-04-13 16:08:48 -0300

    dando estilo

--- db/producto.js
+++ db/producto.js
@@ -8,54 +8,63 @@ const producto={
             date: "03/07/2021"
         },
         {
+            id: 2,
             image: "/images/products/theordinary.jpg",
             product:"Acido Hialuronico The Ordinary",
             description:"Niacinamide 10% + Zinc 1%",
             date: "04/04/2022"
         },
         {
+            id: 3,
             image: "/images/products/limpiadorfacial.jpg",
             product:"Limpiador Facial Cetaphil",
             description:"Loción Limpiadora para Piel Grasa x 237ml",
             date: "02/11/2021"
         },
         {
+            id: 4,
             image: "/images/products/revitalift.jpg",
             product:"Revitalift Loreal",
             description:"Acido Hialuronico",
             date: "19/02/2022"
         },
         {
+            id: 5,
             image: "/images/products/aguaMicelar.jpg",
             product:"Agua Micelar Garnier",
             description:"Agua Micelar Garnier Skin Active x 400 ml",
             date: "11/04/2022"
         },
         {
+            id: 6,
             image: "/images/products/clearwash.png",
             product:"Clear Wash Neutrogena",
             description:"Gel limpiador facial Neutrogena",
             date: "24/12/21"
         },
         {
+            id: 7,
             image: "/images/products/neutrogenaHidroboost.jpg",
             product:"Neutrogena Hydro Boost ",
             description:"Gel en crema extra seco",
             date: "06/11/22"
         },
         {
+            id: 8,
             image: "/images/products/chapstick.jpg",
             product:"Balsamo labial Chap Stick ",
             description:"Balsamo labial clasico",
             date: "06/05/2020"
         },
         {
+            id: 9,
             image: "/images/products/toallitasdesmaquillantes.jpg",
             product:"Removedor de maquillaje Neutrogena",
             description:"Toallas humedas limpiadoras de maquillaje",
             date: "22/11/2021"
         },
         {
+            id: 10,
             image: "/images/products/eucerin.jpg",
             product:"Protector solar Eucerin Oil Control",
             description:"Toque Seco en gel-crema FPS50 x 50 ml",
