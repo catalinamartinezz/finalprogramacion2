f38ff6402365244b58694d06f64030b6a0029e67
Maria Sicco <mjsicco@hotmail.com>
2022-04-15 18:23:59 -0300

    estilos

--- db/producto.js
+++ db/producto.js
@@ -3,70 +3,70 @@ const producto={
         {
             id: 1,
             image: "/images/products/fotoproducto.png",
-            product:"Labial Carmex - Balsamo de labios",
+            product:"LABIAL CARMEX - BALSAMO DE LABIOS",
             description:"Bálsamo reparador de labios presentacion en tubo por unidad (no blister)",
             date: "03/07/2021"
         },
         {
             id: 2,
             image: "/images/products/theordinary.jpg",
-            product:"Acido Hialuronico The Ordinary",
+            product:"ACIDO HIALURONICO THE ORDINARY",
             description:"Niacinamide 10% + Zinc 1%",
             date: "04/04/2022"
         },
         {
             id: 3,
             image: "/images/products/limpiadorfacial.jpg",
-            product:"Limpiador Facial Cetaphil",
+            product:"LIMPIADOR FACIAL CETAPHIL",
             description:"Loción Limpiadora para Piel Grasa x 237ml",
             date: "02/11/2021"
         },
         {
             id: 4,
             image: "/images/products/revitalift.jpg",
-            product:"Revitalift Loreal",
+            product:"REVITALIFT LOREAL",
             description:"Acido Hialuronico",
             date: "19/02/2022"
         },
         {
             id: 5,
             image: "/images/products/aguaMicelar.jpg",
-            product:"Agua Micelar Garnier",
+            product:"AGUA MICELARG GARNIER ",
             description:"Agua Micelar Garnier Skin Active x 400 ml",
             date: "11/04/2022"
         },
         {
             id: 6,
             image: "/images/products/clearwash.png",
-            product:"Clear Wash Neutrogena",
+            product:"CLEAR WASH NEUTROGENA",
             description:"Gel limpiador facial Neutrogena",
             date: "24/12/21"
         },
         {
             id: 7,
             image: "/images/products/neutrogenaHidroboost.jpg",
-            product:"Neutrogena Hydro Boost ",
+            product:"NEUTROGENA HYDRO BOOST ",
             description:"Gel en crema extra seco",
             date: "06/11/22"
         },
         {
             id: 8,
             image: "/images/products/chapstick.jpg",
-            product:"Balsamo labial Chap Stick ",
+            product:"BALSAMO LABIAL CHAP STICK ",
             description:"Balsamo labial clasico",
             date: "06/05/2020"
         },
         {
             id: 9,
             image: "/images/products/toallitasdesmaquillantes.jpg",
-            product:"Removedor de maquillaje Neutrogena",
+            product:"REMOVEDOR DE MAQUILLAJE NEUTROGENA",
             description:"Toallas humedas limpiadoras de maquillaje",
             date: "22/11/2021"
         },
         {
             id: 10,
             image: "/images/products/eucerin.jpg",
-            product:"Protector solar Eucerin Oil Control",
+            product:"PROTECTOR SOLAREUCERIN OIL CONTROL",
             description:"Toque Seco en gel-crema FPS50 x 50 ml",
             date: "11/10/2022"
         },
