90006de0b03e06f609d62afd48c469a7377a81f6
luisigarciia <garcial@udesa.edu.ar>
2022-04-17 15:50:42 -0300

    arreglos en productadd

--- routes/product.js
+++ routes/product.js
@@ -1,11 +1,9 @@
-const express = require('express');
+const express = require ("express");
 const router = express.Router();
 const productController = require("../controllers/productController");
 
-/* GET home page. */
-router.get('/:id?', productController.index);
-router.get('/productadd', productController.productAdd);
-router.get('/searchresults', productController.searchResults);
+router.get("/product/:id?", productController.index);
+router.get("/productadd", productController.productAdd);
+router.get("/searchresults", productController.searchResults);
 
-
-module.exports = router;
+module.exports= router;
